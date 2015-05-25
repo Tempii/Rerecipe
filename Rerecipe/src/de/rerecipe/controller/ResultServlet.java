0add232663ee205176aa5cf0280af9fc3e7dd3f7
Kai_Rechner <kai.von.holdt@uni-oldenburg.de>
2015-05-25 20:57:15 +0200

    Rezepteseite gefixt

--- Rerecipe/src/de/rerecipe/controller/ResultServlet.java
+++ Rerecipe/src/de/rerecipe/controller/ResultServlet.java
@@ -23,13 +23,14 @@ import de.rerecipe.persistence.RecipesDatabase;
 @WebServlet("/Main")
 public class ResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public ResultServlet() {
 		super();
 	}
-	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -38,24 +39,25 @@ public class ResultServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		String[] filter = request.getParameterValues("filter[]");
 		String search1 = "ing:";
-		String search2 = "filter:";		
+		String search2 = "filter:";
 		String queryString = request.getQueryString();
-		if (queryString != null)
+		if (!queryString.isEmpty())
 			if (queryString.startsWith("filter"))
 				queryString = "";
+			else if (filter != null)
+				queryString = queryString.substring(0,
+						queryString.indexOf("&filter"));
 			else
-				queryString = queryString.substring(0, queryString.indexOf("&filter"));
-		else
-			queryString = "";
+				queryString = "";
 		search1 += queryString + "_";
-				
+
 		if (filter != null)
-			for (String option : filter) 
+			for (String option : filter)
 				search2 += option + "&";
 		if (search2.lastIndexOf("&") > -1)
-			search2 = search2.substring(0,search2.lastIndexOf("&"));
-		response.sendRedirect("result.html?"+search1+search2);
-		
+			search2 = search2.substring(0, search2.lastIndexOf("&"));
+		response.sendRedirect("result.html?" + search1 + search2);
+
 	}
 
 	/**
@@ -64,23 +66,24 @@ public class ResultServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
+
 		String FilterHtml = "";
-		List<RecipeResult> recipeResult = new ArrayList<RecipeResult>();	
-		
+		List<RecipeResult> recipeResult = new ArrayList<RecipeResult>();
+
 		String ingredients = request.getParameter("ingredients");
 		String filterString = request.getParameter("filter");
-			
+
 		List<String> ingNames = new ArrayList<String>();
 		List<String> ingAmount = new ArrayList<String>();
 		List<String> filter = new ArrayList<String>();
-		
+
 		PrintWriter writer = response.getWriter();
-		
+
 		int i = 0;
 		while (filterString.contains("&")) {
-			String option = filterString.substring(0, filterString.indexOf("&"));
-			filterString = filterString.replace(option+"&", "");
+			String option = filterString
+					.substring(0, filterString.indexOf("&"));
+			filterString = filterString.replace(option + "&", "");
 			filter.add(option);
 			FilterHtml += option + "<br>";
 		}
@@ -88,61 +91,74 @@ public class ResultServlet extends HttpServlet {
 			FilterHtml += filterString + "<br>";
 			filter.add(filterString);
 		}
-			
-		
+
 		while (ingredients.contains("&")) {
 			String option = ingredients.substring(0, ingredients.indexOf("&"));
-			ingredients = ingredients.replace(option+"&", "");
-			ingNames.add(option.substring(0,option.indexOf("=")));
-			ingAmount.add(option.substring(option.indexOf("=")+1,option.length()));
+			ingredients = ingredients.replace(option + "&", "");
+			ingNames.add(option.substring(0, option.indexOf("=")));
+			ingAmount.add(option.substring(option.indexOf("=") + 1,
+					option.length()));
 		}
 		if (ingredients.length() > 0) {
-			ingNames.add(ingredients.substring(0,ingredients.indexOf("=")));
-			ingAmount.add(ingredients.substring(ingredients.indexOf("=")+1,ingredients.length()));
+			ingNames.add(ingredients.substring(0, ingredients.indexOf("=")));
+			ingAmount.add(ingredients.substring(ingredients.indexOf("=") + 1,
+					ingredients.length()));
 		}
-		
+
 		// Hier sollte man die Liste aus der DB holen
-		recipeResult = RecipesDatabase.getRecipeResults(filter,ingNames);
-		
+		recipeResult = RecipesDatabase.getRecipeResults(filter, ingNames);
+
 		writer.println("<tr><th>Zutat</th><th>Menge</th><th>Einheit</th>");
 		if (ingNames.size() > 0) {
-			for (int j = 0; j< ingNames.size();j++)
-				writer.println("<tr><td>"+ingNames.get(j)+"</td><td>"+ingAmount.get(j)+"</td><td>g/ml/stk</td></tr>");
+			for (int j = 0; j < ingNames.size(); j++)
+				writer.println("<tr><td>" + ingNames.get(j) + "</td><td>"
+						+ ingAmount.get(j) + "</td><td>g/ml/stk</td></tr>");
 		}
 		writer.println("%0");
-		
+
 		// Den Filter aufbauen
-		if (FilterHtml != "")	
+		if (FilterHtml != "")
 			writer.println("<td>" + FilterHtml + "</td>");
 		writer.println("%1");
 		// Alle Rezepte anzeigen
-		if (recipeResult != null) {		             
+		if (recipeResult != null) {
 			for (RecipeResult item : recipeResult) {
 				writer.println("<td><div id=template>");
 				writer.println("<div id=name>" + item.getName() + "</div>");
-				writer.println("<a href=\"recipe.html?r_id=" + item.getId() + "\" id=\""+ item.getId() + "\" onclick=\"document.location=recipe.html?r_id=this.id+'';return false;\" ><img alt="+item.getId()+" src=" + item.getPicture() + "></a>");
-				writer.println("<div id=ratingBox align=left><div style=\"background-color:#f7931e; height:20px;  width:"+(item.getRating()/5)*100+"px;\"><img src=\"img/ratingboxsmall.png\"></div></div>");
+				writer.println("<a href=\"recipe.html?r_id="
+						+ item.getId()
+						+ "\" id=\""
+						+ item.getId()
+						+ "\" onclick=\"document.location=recipe.html?r_id=this.id+'';return false;\" ><img alt="
+						+ item.getId() + " src=" + item.getPicture() + "></a>");
+				writer.println("<div id=ratingBox align=left><div style=\"background-color:#f7931e; height:20px;  width:"
+						+ (item.getRating() / 5)
+						* 100
+						+ "px;\"><img src=\"img/ratingboxsmall.png\"></div></div>");
 				if (item.getIngredients().size() > 0) {
 					writer.println("<div id=IngText> Es fehlt ihnen: ");
-					for (Ingredient ing : item.getIngredients()) // Hier eventuell einen Counter um maximale Einträge zu erzielen
-						
-						if (item.getIngredients().indexOf(ing) == item.getIngredients().size()-1)
-							writer.println(ing.getName() + ".");						
+					for (Ingredient ing : item.getIngredients())
+						// Hier eventuell einen Counter um maximale Einträge zu
+						// erzielen
+
+						if (item.getIngredients().indexOf(ing) == item
+								.getIngredients().size() - 1)
+							writer.println(ing.getName() + ".");
 						else
-							writer.println(ing.getName() + ", ");					
-				} else 
+							writer.println(ing.getName() + ", ");
+				} else
 					writer.println("Sie haben alle Zutaten.");
-				
+
 				writer.println("<button id=teilenButton>teilen</button>");
-				
-				if (i%5==4)
+
+				if (i % 5 == 4)
 					writer.println("<tr>");
-				
+
 				writer.println("</div>");
-				writer.print("</td>");	
+				writer.print("</td>");
 				i++;
 			}
-		} 
+		}
 		writer.println("%2");
 		writer.close();
 	}
